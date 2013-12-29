77a1769c04fe435f9085c6655203ca1afe357512
Robert Martin <robertmartinbc@shaw.ca>
2013-12-29 14:39:08 -0800

    First commit and README update.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a site dedicated to the memory of Vishnoo Paupiah, who passed away on 26th December 2013. This will be for family members and close friends to pay tribute to Vishnoo.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vishnoopaupiah::Application.config.secret_token = 'cfb607fb4b39ec4ddc93940fe619534144d6e6c398ff31d0b366e43b153be3ebb60c6d42edddc81050579d9b46c089da72a85488578e0f090038ee2a8f15158a'
